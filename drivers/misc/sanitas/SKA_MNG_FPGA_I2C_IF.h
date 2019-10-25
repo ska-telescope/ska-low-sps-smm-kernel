49a431f139026e068bf8ce2bd099088a31d4866c
cristian.albanese <cristian.albanese@sanitaseg.it>
2019-10-25 11:16:48 +0200

    changed FPGAI2C Regs

--- drivers/misc/sanitas/SKA_MNG_FPGA_I2C_IF.h
+++ drivers/misc/sanitas/SKA_MNG_FPGA_I2C_IF.h
@@ -6,25 +6,17 @@
 #undef REGISTER_REG_NAME
 
 #include "REGISTER_RESET.h"
-#define REGISTER_DESCRIPTION " R - Max 16B"
+#define REGISTER_DESCRIPTION "command register: routeradd+bytenum2read+bytenum2write+ICadd"
 #define REGISTER_MODE_READ
 #define REGISTER_MODE_WRITE
 #define REGISTER_CMDS {0x00, 0, 0, 32}
-#include "REGISTER_MAKE.h"
-DEFECATE(twi_command_byte_len)
-
-#include "REGISTER_RESET.h"
-#define REGISTER_DESCRIPTION "command register: bytenum2read+bytenum2write+routeradd+ICadd"
-#define REGISTER_MODE_READ
-#define REGISTER_MODE_WRITE
-#define REGISTER_CMDS {0x04, 0, 0, 32}
 #include "REGISTER_MAKE.h"
 DEFECATE(twi_command)
 
 #include "REGISTER_RESET.h"
 #define REGISTER_DESCRIPTION "status register"
 #define REGISTER_MODE_READ
-#define REGISTER_CMDS {0x08, 0, 0, 32}
+#define REGISTER_CMDS {0x0C, 0, 0, 32}
 #include "REGISTER_MAKE.h"
 DEFECATE(twi_status)
 
@@ -32,7 +24,7 @@ DEFECATE(twi_status)
 #define REGISTER_DESCRIPTION "int register"
 #define REGISTER_MODE_READ
 #define REGISTER_MODE_WRITE
-#define REGISTER_CMDS {0x0C, 0, 0, 32}
+#define REGISTER_CMDS {0x10, 0, 0, 32}
 #include "REGISTER_MAKE.h"
 DEFECATE(twi_irq)
 
@@ -40,7 +32,7 @@ DEFECATE(twi_irq)
 #define REGISTER_DESCRIPTION "int en register"
 #define REGISTER_MODE_READ
 #define REGISTER_MODE_WRITE
-#define REGISTER_CMDS {0x10, 0, 0, 32}
+#define REGISTER_CMDS {0x14, 0, 0, 32}
 #include "REGISTER_MAKE.h"
 DEFECATE(twi_irq_en)
 
@@ -48,7 +40,7 @@ DEFECATE(twi_irq_en)
 #define REGISTER_DESCRIPTION "Write Data Register (Add + data)"
 #define REGISTER_MODE_READ
 #define REGISTER_MODE_WRITE
-#define REGISTER_CMDS {0x100, 0, 0, 32}
+#define REGISTER_CMDS {0x4, 0, 0, 32}
 #include "REGISTER_MAKE.h"
 DEFECATE(twi_wrdata)
 
@@ -56,7 +48,7 @@ DEFECATE(twi_wrdata)
 #define REGISTER_DESCRIPTION "Read data Register"
 #define REGISTER_MODE_READ
 #define REGISTER_MODE_WRITE
-#define REGISTER_CMDS {0x200, 0, 0, 32}
+#define REGISTER_CMDS {0x08, 0, 0, 32}
 #include "REGISTER_MAKE.h"
 DEFECATE(twi_rdata)
 
@@ -68,7 +60,6 @@ DEFECATE(twi_rdata)
 #define REGISTER(__name__) register_ ## __name__
 static const Register * SKA_MNG_FPGA_I2C_registers[] = {
 	//static const Registers registers = {
-	&(REGISTER(twi_command_byte_len)),
 	&(REGISTER(twi_command)),
 	&(REGISTER(twi_status)),
 	&(REGISTER(twi_irq)),
@@ -82,7 +73,6 @@ static const Register * SKA_MNG_FPGA_I2C_registers[] = {
 #ifdef REGISTER_MAKE_SYSFS
 #define REGISTER(__name__) register_dev_attr_ ## __name__.dev_attr.attr
 static struct attribute* SKA_MNG_FPGA_I2C_attributes[] = {
-	&(REGISTER(twi_command_byte_len)),
 	&(REGISTER(twi_command)),
 	&(REGISTER(twi_status)),
 	&(REGISTER(twi_irq)),
